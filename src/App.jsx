30582a84407942571ce5d2667b409286f40bb8ba
harsh23049 <cse23049@iiitkalyani.ac.in>
2026-06-06 22:48:01 +0530

    feat: add initial project setup with React, Vite, and Tailwind CSS
    - Created main application structure with App component.
    - Added CSS styles for various components including counter and hero sections.
    - Introduced SVG icons for social media and documentation.
    - Configured Vite for React and Tailwind CSS integration.
    - Added assets including hero image and logos for React and Vite.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className=' text-3xl text-center my-3 bg-orange-500'>Currency Converter</h1>
+    </>
+  )
+}
+
+export default App
